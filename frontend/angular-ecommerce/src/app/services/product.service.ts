0d4bdabaa47c6ad00308a269a9b4e3cb21fe521c
crystalchavez99 <redfire1205@hotmail.com>
2023-08-17 16:05:02 -0700

    create service to call rest api for product

--- frontend/angular-ecommerce/src/app/services/product.service.ts
+++ frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {map, Observable} from "rxjs";
+import {Product} from "../common/product";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private baseUrl = "http://localhost:8080/api/products";
+
+
+  // performs HTTP Requests
+  constructor(private httpClient: HttpClient) { }
+
+  // returns an observable, maps json data from spring data rest to product array
+  getProductList(): Observable<Product[]>{
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    )
+  }
+}
+
+// unwraps json from spring data rest _embedded entry
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  }
 }
