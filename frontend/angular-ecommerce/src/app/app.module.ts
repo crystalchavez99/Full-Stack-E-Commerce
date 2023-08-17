8e014fe26c355dcde6a276e8e842919d3ee31a4e
crystalchavez99 <redfire1205@hotmail.com>
2023-08-17 15:42:21 -0700

    create product class and service and update our app module

--- frontend/angular-ecommerce/src/app/app.module.ts
+++ frontend/angular-ecommerce/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ProductService} from "./services/product.service";
 
 @NgModule({
   declarations: [
@@ -10,9 +12,12 @@ import { ProductListComponent } from './components/product-list/product-list.com
     ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  // adds references to our service
+  //allows us to inject our given service to our app
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
