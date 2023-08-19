c4282601ac386679de5592884450db98ed01c3fd
crystalchavez99 <redfire1205@hotmail.com>
2023-08-18 22:45:53 -0700

    build html table

--- frontend/angular-ecommerce/src/app/common/product.ts
+++ frontend/angular-ecommerce/src/app/common/product.ts
@@ -6,7 +6,7 @@ export class Product {
               public unitPrice:  number,
               public imageUrl: string,
               public active: boolean,
-              public unitsIntStock: number,
+              public unitsInStock: number,
               public dateCreated: Date,
               public lastUpdated: Date
   ) {
